1262538dcb3e62c4b999b59e0cca03c4865fc9f0
JuaniBurgos <juanburgos86b@gmail.com>
2023-07-17 21:45:55 -0300

    Punto 01 Corregido y reazalizado de 2 manera

--- js/equipo04.js
+++ js/equipo04.js
@@ -1,5 +1,13 @@
 const meses =['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
+let losMeses='';
+for (let i = 0; i < meses.length; i++) {
+      losMeses += "-" + meses [i] + "\n";
+    }
+alert("Los meses:\n" + losMeses);
 
-for(let i=0;i < meses.length;i++){
-    console.log('- '+meses[i]);
-}
+
+// Forma 02
+// const verMeses = () =>{
+// alert(meses.join('\n'))
+// }
+// verMeses();
